697045059cda5a3ffcdfe4e23c1844da344f3b57
خزيمة الدنيا <35159023+AmyrAlDunya@users.noreply.github.com>
2018-01-12 21:32:40 +0800

    Add files via upload

---
+++ KhuzaimahAdDetector.js
@@ -0,0 +1,14 @@
+var KhuzaimahAdDetector = {
+ IsAdBlockDetected: function( ) {
+  var e = document.createElement('div');
+  e.id = 'PsiFdanMtBql';
+  document.body.appendChild( e );
+
+  if( document.getElementById('PsiFdanMtBql') ) {
+   return false;
+  }
+  else {
+   return true;
+  }
+ }
+};
